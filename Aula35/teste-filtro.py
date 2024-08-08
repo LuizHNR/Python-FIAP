1bd048b61634c5cace2f64d076db0b2a37045162
LuizHNR <luizhneri12@gmail.com>
2024-08-08 11:14:29 -0300

    filtro

---
+++ Aula35/teste-filtro.py
@@ -0,0 +1,12 @@
+lista = [1,2,3,4,5,6,7,8,9,10]
+
+#Numeros Impares
+for numero in lista:
+    if numero % 2 != 0:
+        print(numero)
+        
+#Numeros Pares
+for numero in lista:
+    if numero % 2 == 0:
+        print(numero)
+        
