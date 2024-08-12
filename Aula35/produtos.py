9544805e5f2658b0643ca319c6413b249c8d037c
LuizHNR <luizhneri12@gmail.com>
2024-08-12 10:40:34 -0300

    conclusao

--- Aula35/produtos.py
+++ Aula35/produtos.py
@@ -1,13 +1,10 @@
-produtos = [ {"nome": "Camisa", "preço": 50, "em_estoque": True}, 
-            {"nome": "Calça", "preço": 80, "em_estoque": False}, 
-            {"nome": "Chapéu", "preço": 30, "em_estoque": True}, ] 
+produtos = [ {"nome": "Camisa", "preco": 50, "em_estoque": True}, 
+            {"nome": "Calça", "preco": 80, "em_estoque": False}, 
+            {"nome": "Chapéu", "preco": 30, "em_estoque": True}, ] 
 
-def estoque_verdadeiro(estoque):
-    for estoque in produtos:
-        if estoque["em_estoque"] != True:
-            return False
-        else:
-            return True
+def estoque_verdadeiro(produtos):
+    if produtos ["em_estoque"] == True:
+        return True
     
 nova_lista = filter(estoque_verdadeiro, produtos)
 
