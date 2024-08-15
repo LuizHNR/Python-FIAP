92fe7027bb36ec1b0392c3e4ca45b3bda73bb6db
LuizHNR <luizhneri12@gmail.com>
2024-08-15 10:59:53 -0300

    exercicio

--- aula37/app-nomes.py
+++ aula37/app-nomes.py
@@ -5,26 +5,60 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
 class AppNome( App ):
-    
-    txtNome = Nome
+
+    txt_nome = TextInput(size_hint=(0.7, 1.0))
+    txt_telefone = TextInput(size_hint=(0.7, 1.0))
+    txt_email = TextInput(size_hint=(0.7, 1.0))
+    txt_nascimento = TextInput(size_hint=(0.7, 1.0))
 
     def submit(self, e):
         print("Botão pressionado")
-        print(self.txtNome.text)
+        print(self.txt_nome.text)
+        print(self.txt_telefone.text)
+        print(self.txt_email.text)
+        print(self.txt_nascimento.text)
 
     def build(self):
         box = BoxLayout(orientation="vertical")
-        boxNome = BoxLayout(orientation="horizontal",
+        box_nome = BoxLayout(orientation="horizontal",
                             size_hint = (1.0, 0.2))
-        lblNome = Label(text="Digite um nome", 
-                        size_hint = (0.3, 1.0))
-        self.txtNome = TextInput(size_hint = (0.7, 1.0))
-        btnSalvar = Button(text="Salvar", size_hint = (1, 1),on_release = self.submit)
+        box_telefone = BoxLayout(orientation="horizontal",
+                                size_hint=(1.0, 0.2))
+    
+        box_email = BoxLayout(orientation="horizontal",
+                                size_hint=(1.0, 0.2))
+        
+        box_nascimento = BoxLayout(orientation="horizontal",
+                                size_hint=(1.0, 0.2))
 
-        boxNome.add_widget(lblNome)
-        boxNome.add_widget(self.txtNome)
-        box.add_widget(boxNome)
-        box.add_widget(btnSalvar)
+        lbl_nome = Label(text="Nome:", 
+                        size_hint = (0.3, 1.0))
+        lbl_telefone = Label(text="Telefone: ", 
+                            size_hint = (0.3, 1.0))
+        
+        lbl_email = Label(text="email: ", 
+                            size_hint = (0.3, 1.0))
+        
+        lbl_nascimento = Label(text="Data de nascimento: ", 
+                            size_hint = (0.3, 1.0))
+        
+        btn_salvar = Button(text="Salvar", 
+                           size_hint = (1, 1),
+                           on_release = self.submit)
+        
+        box_nome.add_widget(lbl_nome)
+        box_nome.add_widget(self.txt_nome)
+        box_telefone.add_widget(lbl_telefone)
+        box_telefone.add_widget(self.txt_telefone)
+        box_email.add_widget(lbl_email)
+        box_email.add_widget(self.txt_email)
+        box_nascimento.add_widget(lbl_nascimento)
+        box_nascimento.add_widget(self.txt_nascimento)
+        box.add_widget(box_nome)
+        box.add_widget(box_telefone)
+        box.add_widget(box_email)
+        box.add_widget(box_nascimento)
+        box.add_widget(btn_salvar)
         return box
     
 
