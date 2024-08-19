72b8117ed18e17409dd4a14c35ffe1feb3b60d51
LuizHNR <luizhneri12@gmail.com>
2024-08-19 09:37:02 -0300

    uso do stack e head

--- Aula38/explodir_heap.py
+++ Aula38/explodir_heap.py
@@ -1,7 +1,7 @@
-
+contador = 1
 
 class No:
-    lista = [x for x in range(1000)]
+    lista = [x for x in range(100000000)]
     proximo = None
 
 if __name__ == "__main__":
@@ -9,6 +9,9 @@ if __name__ == "__main__":
     raiz = No()
     temp = raiz
     while True:
+        contador = contador + 1
+        print(f"Contador: {contador}")
         outro = No()
         temp.proximo = outro
+        temp = outro
 
