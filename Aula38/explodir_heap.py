2ac5f5d3c54afa0c05859f2d37300c5478ff1d18
LuizHNR <luizhneri12@gmail.com>
2024-08-19 09:28:03 -0300

    .

--- Aula38/explodir_heap.py
+++ Aula38/explodir_heap.py
@@ -2,7 +2,13 @@
 
 class No:
     lista = [x for x in range(1000)]
+    proximo = None
 
 if __name__ == "__main__":
 
     raiz = No()
+    temp = raiz
+    while True:
+        outro = No()
+        temp.proximo = outro
+
