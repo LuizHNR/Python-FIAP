894b9daeb59fc3e2d3961dace1c70ef1a1f7fbdb
LuizHNR <luizhneri12@gmail.com>
2024-09-09 09:40:39 -0300

    ler,procurar

---
+++ Aula44/ler.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+url = "https://tdspm-luiz-default-rtdb.firebaseio.com/contatos.json"
+
+response = requests.get(url)
+
+texto =  response.text
+
+print("Response: ", response)
+print("Corpo: ", texto)
+
+contatos = json.loads( texto )
+
+print("Chaves dos contatos: \n")
+# for chave in contatos.keys():
+#     print(chave)
+
+for contato in contatos.values():
+    print(f"Nome: {contato['nome']}\tEmail: {contato['email']}\tTelefone: {contato['telefone']}")
